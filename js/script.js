dc997c36d17710382018f6c9f160b18340434236
Daniela Forero Ballen <dani.didi.fb@gmail.com>
2024-04-22 22:04:53 -0500

    feat: :art: Agregar código a script.js
    Se agregó más código para permitir la visualización de los datos dentro del api.

--- js/script.js
+++ js/script.js
@@ -18,12 +18,52 @@ function fetchSuperHero(){
     xhr.send();
 }
 function displayHero(data){
+
     let heroInfo = document.getElementById("superHeroInfo");
     if (data.response === "error"){
         heroInfo.innerHTML = `<p>Error: ${data.error}</p>`
     } else{
+        stats = data.powerstats
+        img_url = data.image
+        work = data.work
+        connections = data.connections
+        affiliation = connections["group-affiliation"]
+        appearance = data.appearance
+        eye_color = appearance["eye-color"]
+        hair_color = appearance["hair-color"]
+        
+
         heroInfo.innerHTML = `
+
+        <h3>General Info</h3>
         <p>Name: ${data.name}</p>
+        <br><img src="${img_url.url}"</br>
+
+        <h3>Powerstats</h3>
+        <p>Combat: ${stats.combat}</p>
+        <p>Durability: ${stats.durability}</p>
+        <p>Intelligence: ${stats.intelligence}</p>
+        <p>Power: ${stats.power}</p>
+        <p>Speed: ${stats.speed}</p>
+        <p>Strength: ${stats.strength}</p>
+
+        <h3>Work</h3>
+        <p>Occupation: ${work.occupation}</p>
+        <p>Base: ${work.base}</p>
+
+        <h3>Connections</h3>
+        <p>Group/Affiliation: ${affiliation}</p>
+        <p>Relatives: ${connections.relatives}</p>
+
+        <h3>Appareance</h3>
+        <p>Eye color: ${eye_color}</p>
+        <p>Gender: ${appearance.gender}</p>
+        <p>Hair color: ${hair_color}</p>
+        <p>Race: ${appearance.race}</p>
+        <p>Height: ${appearance.height[0]} or ${appearance.height[1]}</p>
+        <p>Weight: ${appearance.weight[0]} or ${appearance.weight[1]}</p>
+
+
         `
     }
 }
