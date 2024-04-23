9f87cc06492fad4abeb61d5fa2d3d3569f510f54
Daniela Forero Ballen <dani.didi.fb@gmail.com>
2024-04-22 22:08:42 -0500

    feat: :art: Finalizar agregar información de lectura
    Se completó el codigo de impresión para la información del api

--- js/script.js
+++ js/script.js
@@ -28,16 +28,25 @@ function displayHero(data){
         work = data.work
         connections = data.connections
         affiliation = connections["group-affiliation"]
+
+        //APPEARANCE
         appearance = data.appearance
         eye_color = appearance["eye-color"]
         hair_color = appearance["hair-color"]
         
+        //BIO
+        biography = data.biography
+        alter_ego = biography["alter-ego"]
+        first_appearance = biography["first-appearance"]
+        full_name = biography["full-name"]
+        place_of_birth = biography["place-of-birth"]
 
         heroInfo.innerHTML = `
 
         <h3>General Info</h3>
         <p>Name: ${data.name}</p>
         <br><img src="${img_url.url}"</br>
+
 
         <h3>Powerstats</h3>
         <p>Combat: ${stats.combat}</p>
@@ -47,22 +56,34 @@ function displayHero(data){
         <p>Speed: ${stats.speed}</p>
         <p>Strength: ${stats.strength}</p>
 
+
         <h3>Work</h3>
         <p>Occupation: ${work.occupation}</p>
         <p>Base: ${work.base}</p>
 
+
         <h3>Connections</h3>
         <p>Group/Affiliation: ${affiliation}</p>
         <p>Relatives: ${connections.relatives}</p>
+
 
         <h3>Appareance</h3>
         <p>Eye color: ${eye_color}</p>
         <p>Gender: ${appearance.gender}</p>
         <p>Hair color: ${hair_color}</p>
         <p>Race: ${appearance.race}</p>
-        <p>Height: ${appearance.height[0]} or ${appearance.height[1]}</p>
-        <p>Weight: ${appearance.weight[0]} or ${appearance.weight[1]}</p>
+        <p>Height: ${appearance.height[0]}, ${appearance.height[1]}</p>
+        <p>Weight: ${appearance.weight[0]}, ${appearance.weight[1]}</p>
 
+
+        <h3>Biography</h3>
+        <p>Full name: ${full_name}</p>
+        <p>Aliases: ${biography.aliases}</p>
+        <p>Publisher: ${biography.publisher}</p>
+        <p>First appearance: ${first_appearance}</p>
+        <p>Place of birth: ${place_of_birth}</p>
+        <p>Alignment: ${biography.alignment}</p>
+        <p>Alter ego: ${alter_ego}</p>
 
         `
     }
